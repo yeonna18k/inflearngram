65af78b1265dc07ecd69594c8e43b233e8ae2350
yeonna18k <yeonna18k@gmail.com>
2024-07-24 17:47:37 +0900

    feat/ realtime chatting complete

--- components/chat/ChatScreen.tsx
+++ components/chat/ChatScreen.tsx
@@ -1,21 +1,68 @@
 "use client";
 
-import { Button } from "@material-tailwind/react";
+import { Spinner } from "@material-tailwind/react";
 import Person from "./Person";
 import Message from "./Message";
 import { useRecoilValue } from "recoil";
 import { selectedUserIdState, selectedUserIndexState } from "utils/recoil/atom";
-import { useQuery } from "@tanstack/react-query";
-import { getUserById } from "actions/chatActions";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllMessages, getUserById, sendMessage } from "actions/chatActions";
+import { useEffect, useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function ChatScreen() {
   const selectedUserId = useRecoilValue(selectedUserIdState);
   const selectedUserIndex = useRecoilValue(selectedUserIndexState);
+  const [message, setMessage] = useState("");
+
+  const supabase = createBrowserSupabaseClient();
 
   const selectedUserQuery = useQuery({
     queryKey: ["user", selectedUserId],
     queryFn: () => getUserById(selectedUserId),
   });
+
+  const sendMessageMutation = useMutation({
+    mutationFn: async () => {
+      return sendMessage({ message, chatUserId: selectedUserId });
+    },
+    onSuccess: () => {
+      setMessage("");
+      getAllMessgeagesQuery.refetch();
+    },
+  });
+
+  const getAllMessgeagesQuery = useQuery({
+    queryKey: ["messages", selectedUserId],
+    queryFn: () => getAllMessages({ chatUserId: selectedUserId }),
+  });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("message_postgres_changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "message",
+        },
+        (payload) => {
+          if (
+            payload.eventType === "INSERT" &&
+            !payload.errors &&
+            !!payload.new
+          ) {
+            getAllMessgeagesQuery.refetch();
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
 
   return selectedUserQuery.data !== null ? (
     <div className="w-full h-screen flex flex-col">
@@ -29,20 +76,28 @@ export default function ChatScreen() {
         userId={selectedUserQuery.data?.id}
       />
       {/* 채팅 영역 */}
-      <div className="w-full flex-1 flex flex-col p-4 gap-2">
-        <Message isFromMe={true} message={"안녕하세요 반갑습니다"} />
-        <Message isFromMe={false} message={"안녕하세요 저도 반갑습니다"} />
-        <Message isFromMe={true} message={"점심 드셨나요?"} />
-        <Message isFromMe={true} message={"저는 이제 곧 먹으려구요"} />
+      <div className="w-full overflow-y-scroll flex-1 flex flex-col p-4 gap-2">
+        {getAllMessgeagesQuery.data?.map((message) => (
+          <Message
+            key={message.id}
+            message={message.message}
+            isFromMe={message.receiver === selectedUserId}
+          />
+        ))}
       </div>
       {/* 채팅창 영역 */}
       <div className="flex">
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className="w-full p-3 border-2 border-light-blue-600"
           placeholder="메시지를 입력하세요"
         />
-        <button className="min-w-20 p-3 bg-light-blue-600 text-white">
-          <span>전송</span>
+        <button
+          onClick={() => sendMessageMutation.mutate()}
+          className="min-w-20 p-3 bg-light-blue-600 text-white"
+        >
+          {sendMessageMutation.isPending ? <Spinner /> : <span>전송</span>}
         </button>
       </div>
     </div>
