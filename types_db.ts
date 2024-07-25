2410a31d2166c68b967b152b45bd4be67214d4b7
yeonna18k <yeonna18k@gmail.com>
2024-07-25 14:32:21 +0900

    fix/ build bug fix

--- types_db.ts
+++ types_db.ts
@@ -1,114 +1,114 @@
-Need to install the following packages:
-supabase@1.187.3
-Ok to proceed? (y) export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+// Need to install the following packages:
+// supabase@1.187.3
+// Ok to proceed? (y) export type Json =
+//   | string
+//   | number
+//   | boolean
+//   | null
+//   | { [key: string]: Json | undefined }
+//   | Json[]
 
 export type Database = {
   public: {
     Tables: {
       message: {
         Row: {
-          created_at: string
-          id: number
-          is_deleted: boolean
-          message: string
-          receiver: string
-          sender: string
-        }
+          created_at: string;
+          id: number;
+          is_deleted: boolean;
+          message: string;
+          receiver: string;
+          sender: string;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          is_deleted?: boolean
-          message: string
-          receiver: string
-          sender: string
-        }
+          created_at?: string;
+          id?: number;
+          is_deleted?: boolean;
+          message: string;
+          receiver: string;
+          sender: string;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          is_deleted?: boolean
-          message?: string
-          receiver?: string
-          sender?: string
-        }
-        Relationships: []
-      }
+          created_at?: string;
+          id?: number;
+          is_deleted?: boolean;
+          message?: string;
+          receiver?: string;
+          sender?: string;
+        };
+        Relationships: [];
+      };
       movie: {
         Row: {
-          id: number
-          image_url: string
-          overview: string
-          popularity: number
-          release_date: string
-          title: string
-          vote_average: number
-        }
+          id: number;
+          image_url: string;
+          overview: string;
+          popularity: number;
+          release_date: string;
+          title: string;
+          vote_average: number;
+        };
         Insert: {
-          id?: number
-          image_url: string
-          overview: string
-          popularity: number
-          release_date: string
-          title: string
-          vote_average: number
-        }
+          id?: number;
+          image_url: string;
+          overview: string;
+          popularity: number;
+          release_date: string;
+          title: string;
+          vote_average: number;
+        };
         Update: {
-          id?: number
-          image_url?: string
-          overview?: string
-          popularity?: number
-          release_date?: string
-          title?: string
-          vote_average?: number
-        }
-        Relationships: []
-      }
+          id?: number;
+          image_url?: string;
+          overview?: string;
+          popularity?: number;
+          release_date?: string;
+          title?: string;
+          vote_average?: number;
+        };
+        Relationships: [];
+      };
       todo: {
         Row: {
-          completed: boolean
-          created_at: string
-          id: number
-          title: string
-          updated_at: string | null
-        }
+          completed: boolean;
+          created_at: string;
+          id: number;
+          title: string;
+          updated_at: string | null;
+        };
         Insert: {
-          completed: boolean
-          created_at?: string
-          id?: number
-          title: string
-          updated_at?: string | null
-        }
+          completed: boolean;
+          created_at?: string;
+          id?: number;
+          title: string;
+          updated_at?: string | null;
+        };
         Update: {
-          completed?: boolean
-          created_at?: string
-          id?: number
-          title?: string
-          updated_at?: string | null
-        }
-        Relationships: []
-      }
-    }
+          completed?: boolean;
+          created_at?: string;
+          id?: number;
+          title?: string;
+          updated_at?: string | null;
+        };
+        Relationships: [];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -117,23 +117,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+      PublicSchema["Views"])
+  ? (PublicSchema["Tables"] &
+      PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -141,20 +141,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -162,20 +162,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -183,9 +183,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+  : never;
