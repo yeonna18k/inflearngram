ec00ef5785fff788c21b1d6ef066893044747604
yeonna18k <yeonna18k@gmail.com>
2024-08-01 16:58:28 +0900

    chore: remove unnecessary files

--- utils/recoil/atom.ts
+++ utils/recoil/atom.ts
@@ -1,10 +1,5 @@
 import { atom } from "recoil";
 
-export const searchState = atom({
-  key: "searchState",
-  default: "",
-});
-
 export const selectedUserIdState = atom({
   key: "selectedUserIdState",
   default: null,
